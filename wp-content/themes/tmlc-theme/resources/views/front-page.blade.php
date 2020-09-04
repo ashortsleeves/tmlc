3f4341a2d20071a6e9d9fcd684a1f1c16756c816
Adam Shortsleeves <ashortsleeves@gmail.com>
2020-09-04 13:41:02 -0400

    front page hero mobile and desktop

--- wp-content/themes/tmlc-theme/resources/views/front-page.blade.php
+++ wp-content/themes/tmlc-theme/resources/views/front-page.blade.php
@@ -4,19 +4,21 @@
   @while(have_posts()) @php the_post() @endphp
     <section class="fp-hero">
       <div class="container">
-        <svg class="mobile-svg" viewBox="0 0 448 512">
+        <div class="fp-hero-image jumbo-bg" style="background-image: url({{$fp_hero['hero_image']['url']}})">
+
+          <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 306 694">
             <defs>
-                <!-- pattern3 - aspect ratio control on both image and pattern -->
-                <pattern id="pattern3" height="100%" width="100%"
-                patternContentUnits="objectBoundingBox"
-                viewBox="0 0 1 1" preserveAspectRatio="xMidYMid slice" >
-                    <image height="1" width="1"  preserveAspectRatio="xMidYMid slice" href="{!!$fp_hero['hero_image_mobile']['url']!!}"/>
-                </pattern>
+              <style>
+                .cls-1 {
+                  fill: #fff;
+                  fill-rule: evenodd;
+                }
+              </style>
             </defs>
+            <path class="cls-1" d="M5,694H311L41,423c-17.921-17.987-29.492-42.581-31-72-1.522-29.686,10.1-58.722,31-85L306,0H5V694Z" transform="translate(-5)"/>
+          </svg>
 
-          <path d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z" fill="#00a7cf">
-
-        </svg>
+        </div>
         <div class="cta">
           <i class="fas fa-lock"></i>
           <span class="title">{{$fp_hero['title']}}</span>
@@ -31,41 +33,8 @@
             <a class="btn btn-blue" href="{{$fp_hero['button_2']['url']}}">{{$fp_hero['button_2']['title']}}</a>
           @endif
         </div>
-        {{-- <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="lock" class="svg-inline--fa fa-lock fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-          <path fill="currentColor" d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z">
-          </path>
-        </svg> --}}
-
-
-
-        {{-- <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="lock" class="svg-inline--fa fa-lock fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-          <defs>
-              <pattern id="img1" width="100%" height="100%" >
-                  <image href="{{$fp_hero['hero_image']['url']}}" x="-200" y="0"/>
-              </pattern>
-          </defs>
-          <path d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z" fill="url(#img1)" fill="#00a7cf">
-          </path>
-        </svg> --}}
 
       </div>
-
-
-
-
-
-
-{{--
-      <div class="svg-container jumbo-bg" style="background-image: url({{$fp_hero['hero_image']['url']}})">
-        <svg
-         xmlns="http://www.w3.org/2000/svg"
-         xmlns:xlink="http://www.w3.org/1999/xlink"
-         width="1361px" height="2708px">
-        <path fill-rule="evenodd"  fill="rgb(255, 255, 255)"
-         d="M1361.000,2708.000 C1185.333,2708.000 1009.667,2708.000 834.000,2708.000 C556.000,2708.000 278.000,2708.000 0.000,2708.000 C0.000,1805.333 0.000,902.667 0.000,0.000 C453.333,0.000 906.667,0.000 1360.000,0.000 C938.000,422.000 516.000,844.000 94.000,1266.000 C72.264,1289.767 60.124,1320.789 60.000,1353.000 C59.875,1385.555 72.032,1416.978 94.000,1441.000 C516.333,1863.333 938.667,2285.667 1361.000,2708.000 Z"/>
-        </svg>
-
-      </div> --}}
       <div class="fp-strapline">
 
       </div>
