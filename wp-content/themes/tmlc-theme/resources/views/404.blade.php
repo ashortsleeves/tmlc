e7c9dfe7f255f84840f812fd90cae0f9e16b1cd8
ashortsleeves <ashortsleeves@gmail.com>
2020-10-16 14:31:24 -0400

    changes to interior pages and disabling woocommerce pages

--- wp-content/themes/tmlc-theme/resources/views/404.blade.php
+++ wp-content/themes/tmlc-theme/resources/views/404.blade.php
@@ -1,12 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+  <section class="content-section 404-section">
+    <img class="content-section-background" src="{{$footer['background']['url']}}" />
+    <div class="container container-sm container-404">
+      <h1><i class="fas fa-times-hexagon"></i> Page Not Found</h1>
+      <h3>{{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}</h3>
+      <button class="btn btn-lg" onclick="goBack()"><i class="fas fa-arrow-left"></i> Go Back</button>
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+      <script>
+      function goBack() {
+        window.history.back();
+      }
+      </script>
+
     </div>
-    {!! get_search_form(false) !!}
-  @endif
+  </section>
+
 @endsection
